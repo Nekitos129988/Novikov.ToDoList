cb0c6de5abd803e2d27647a8b93de9102a1eea7a
alatyr_chuvsu_vs@outlook.com <alatyr_chuvsu_vs@outlook.com>
2022-03-26 12:43:30 +0300

    Добавление второй формы и интерфейса первой формы

--- WinFormsApp/Form2.cs
+++ WinFormsApp/Form2.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace WinFormsApp
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-		private void label1_Click(object sender, EventArgs e)
+	public partial class Form2 : Form
+	{
+		public Form2()
 		{
-
+			InitializeComponent();
 		}
 	}
 }
