178b18f4d87929c50dedbcdf29b027f8f9affcb0
alatyr_chuvsu_vs@outlook.com <alatyr_chuvsu_vs@outlook.com>
2022-03-26 12:59:49 +0300

    Интерфейс второй формы.

--- WinFormsApp/Form2.Designer.cs
+++ WinFormsApp/Form2.Designer.cs
@@ -29,44 +29,45 @@ namespace WinFormsApp
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
-			// button1
+			// textBox1
 			// 
-			this.button1.Location = new System.Drawing.Point(12, 12);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(52, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.textBox1.BackColor = System.Drawing.Color.Gray;
+			this.textBox1.Location = new System.Drawing.Point(12, 41);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(323, 404);
+			this.textBox1.TabIndex = 2;
 			// 
-			// button2
+			// textBox2
 			// 
-			this.button2.Location = new System.Drawing.Point(70, 12);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(52, 23);
-			this.button2.TabIndex = 1;
-			this.button2.Text = "button2";
-			this.button2.UseVisualStyleBackColor = true;
+			this.textBox2.BackColor = System.Drawing.Color.Gray;
+			this.textBox2.Location = new System.Drawing.Point(12, 12);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(323, 23);
+			this.textBox2.TabIndex = 3;
 			// 
 			// Form2
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.DarkRed;
 			this.ClientSize = new System.Drawing.Size(347, 450);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
 			this.Name = "Form2";
 			this.Text = "Form2";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
 	}
 }
